ff27a49abf9f12f01aa754f15412fb1d5a80d4c3
tocco889-byte <tocco889@gmail.com>
2026-06-08 17:08:58 +0545

    Add files via upload

---
+++ python.py
@@ -0,0 +1,10 @@
+
+a = int(input('enter your number here'))
+b = int(input('enter your number here'))
+c = int(input('choose what you want 1 for multi 2 for add'))
+if c == 1:
+    d = a+b
+    print(d)
+elif c == 2:
+    e = a*b
+    print(e)
